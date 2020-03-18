f99ba737d1289b2b4b71bd97d29b6c8cc0b04ad3
Wil Pirino <wpirino1@gmail.com>
2020-03-18 17:45:23 -0600

    cleaned up styles and added hover effect

--- src/components/pages/landing/Landing.js
+++ src/components/pages/landing/Landing.js
@@ -1,46 +1,34 @@
 import React from "react";
 import { IoIosChatboxes, IoIosPeople, IoIosImages } from "react-icons/io";
 
+const GOOGLE_PLAY_URL = "https://play.google.com/store?hl=en_US";
+const APP_STORE_URL = "https://www.apple.com/ios/app-store/";
+
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center landing-bg min-h-screen text-white px-8 lg:px-32 xl:px-64 py-12">
-      <img
-        className="w-48"
-        src="/imgs/n2m-logo-white.png"
-        alt="Next 2 Me"
-      />
+    <div className="landing-page min-h-screen px-8 lg:px-32 xl:px-64 py-12 flex flex-col items-center text-white">
+      <img className="w-48" src="/imgs/n2m-logo-white.png" alt="Next 2 Me" />
 
       <div className="flex items-center self-stretch justify-around mt-12 md:mt-16">
-        <a
-          className="hidden md:block"
-          href="http://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-        >
-          <img
-            className="w-32"
-            alt="Get it on Google Play"
-            src="/imgs/google-play-button.png"
-          />
+        <a className="app-store-btn hidden md:block" href={GOOGLE_PLAY_URL}>
+          <img alt="Get it on Google Play" src="/imgs/google-play-button.png" />
         </a>
 
         <div className="relative">
           <img
             className="absolute w-40 transform -translate-x-12 -rotate-15"
             src="/imgs/n2m-screenshot-ios8.png"
-            alt="ios 11 screenshot"
+            alt="group chat screenshot"
           />
           <img
             className="w-40 transform translate-x-12 translate-y-2 rotate-5"
             src="/imgs/n2m-screenshot-ios11.png"
-            alt="ios 11 screenshot"
+            alt="join group screenshot"
           />
         </div>
 
-        <a
-          className="hidden md:block"
-          href="https://www.apple.com/ios/app-store/"
-        >
+        <a className="app-store-btn hidden md:block" href={APP_STORE_URL}>
           <img
-            className="w-32"
             alt="Download on the App Store"
             src="/imgs/app-store-button.svg"
           />
@@ -48,16 +36,11 @@ const Landing = () => {
       </div>
 
       <div className="md:hidden flex justify-around self-stretch mt-12">
-        <a href="http://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-          <img
-            className="w-32"
-            alt="Get it on Google Play"
-            src="/imgs/google-play-button.png"
-          />
+        <a className="app-store-btn" href={GOOGLE_PLAY_URL}>
+          <img alt="Get it on Google Play" src="/imgs/google-play-button.png" />
         </a>
-        <a href="https://www.apple.com/ios/app-store/">
+        <a className="app-store-btn" href={APP_STORE_URL}>
           <img
-            className="w-32"
             alt="Download on the App Store"
             src="/imgs/app-store-button.svg"
           />
@@ -65,22 +48,22 @@ const Landing = () => {
       </div>
 
       <ul className="flex flex-col sm:flex-row mt-12 md:mt-16">
-        <li className="flex flex-col items-center text-center text-lg">
-          <IoIosChatboxes className="w-12 h-12 mb-2" />
+        <li className="detail-item">
+          <IoIosChatboxes />
           <p>
             Next2Me is a place to meet new people and talk about what matters to
             you
           </p>
         </li>
-        <li className="flex flex-col items-center text-center my-8 sm:my-0 sm:mx-12 text-lg">
-          <IoIosPeople className="w-12 h-12 mb-2" />
+        <li className="detail-item my-8 sm:my-0 sm:mx-12">
+          <IoIosPeople />
           <p>
             Chat with someone completely random or someone who shares your
             interests
           </p>
         </li>
-        <li className="flex flex-col items-center text-center text-lg">
-          <IoIosImages className="w-12 h-12 mb-2" />
+        <li className="detail-item">
+          <IoIosImages />
           <p>Share photos, video, and audio with the people that you meet</p>
         </li>
       </ul>
