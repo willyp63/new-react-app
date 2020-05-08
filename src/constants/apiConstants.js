d615a0d7c5042ab1808f83601c706709b510a9f6
Wil Pirino <wpirino1@gmail.com>
2020-05-08 15:28:23 -0600

    changed api url

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,2 +1,2 @@
 /** Url to pull the initial set of events from */
-export const API_URL = "http://localhost:8080/events.json";
+export const API_URL = "https://cove-coding-challenge-api.herokuapp.com/reservations";
